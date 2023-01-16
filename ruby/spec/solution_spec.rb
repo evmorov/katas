b2044a5afc3a035c643eea8529a04cca7b419f2c
Evgenii Morozov <evmorov@gmail.com>
2023-01-16 17:54:10 +0100

    Update minitest example

--- ruby/spec/solution_spec.rb
+++ ruby/spec/solution_spec.rb
@@ -1,13 +1,11 @@
 require_relative '../src/solution'
 
 describe 'solution' do
-  describe '#solution' do
-    it 'correct' do
-      expect(solution(1, 3)).to eq(4)
-    end
+  it 'correct' do
+    expect(solution(1, 3)).to eq(4)
+  end
 
-    it 'mistake' do
-      expect(solution(1, 3)).to eq(5)
-    end
+  it 'mistake' do
+    expect(solution(1, 3)).to eq(5)
   end
 end
--- ruby/test/solution_test.rb
+++ ruby/test/solution_test.rb
@@ -3,10 +3,10 @@ require_relative '../src/solution'
 
 class SolutionTest < Minitest::Test
   def test_correct
-    assert_equal 4, sum(1, 3)
+    assert_equal 4, solution(1, 3)
   end
 
   def test_mistake
-    assert_equal 5, sum(1, 3)
+    assert_equal 5, solution(1, 3)
   end
 end
