4a4c0c07a0a00c616f7bf5450ddbc89dc32819a6
Evgenii Morozov <evmorov@gmail.com>
2023-06-29 09:39:41 +0200

    Simple java

---
+++ java/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+  public int solution(int a, int b) {
+    return a + b;
+  }
+}
+
---
+++ java/Test.java
@@ -0,0 +1,11 @@
+class Test {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+
+    System.out.println("Expected: 4");
+    System.out.println(s.solution(1, 3));
+
+    System.out.println("Expected: 4");
+    System.out.println(s.solution(1, 4));
+  }
+}
