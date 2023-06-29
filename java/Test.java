4c4f4f80a1b1491c88e68f7d3de7c6cd94707129
Evgenii Morozov <evmorov@gmail.com>
2023-06-29 09:57:06 +0200

    Slightly improve java

--- java/Test.java
+++ java/Test.java
@@ -2,10 +2,19 @@ class Test {
   public static void main(String[] args) {
     Solution s = new Solution();
 
-    System.out.println("Expected: 4");
-    System.out.println(s.solution(1, 3));
+    expected(4);
+    actual(s.solution(1, 3));
 
-    System.out.println("Expected: 4");
-    System.out.println(s.solution(1, 4));
+    expected("4");
+    actual(s.solution(1, 4));
+  }
+
+  private static <T> void expected(T s) {
+    System.out.println("Expected: " + s);
+  }
+
+  private static <T> void actual(T s) {
+    System.out.println("  Actual: " + s);
+    System.out.println();
   }
 }
