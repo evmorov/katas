ba6d375ad35da4a36f7e31c408c7c81f447ac0ce
Evgenii Morozov <evmorov@gmail.com>
2023-07-03 09:05:53 +0200

    Swap actual and expected

--- java/Test.java
+++ java/Test.java
@@ -4,28 +4,28 @@ class Test {
   public static void main(String[] args) {
     Solution s = new Solution();
 
-    expected(4);
     actual(s.solution(1, 3));
+    expected(4);
 
-    expected("4");
     actual(s.solution(1, 4));
-  }
-
-  private static <T> void expected(T s) {
-    System.out.println("Expected: " + s);
+    expected("4");
   }
 
   private static <T> void actual(T s) {
     System.out.println("  Actual: " + s);
-    System.out.println();
   }
 
-  private static void expected(double[] s) {
-    System.out.println("Expected: " + Arrays.toString(s));
+  private static <T> void expected(T s) {
+    System.out.println("Expected: " + s);
+    System.out.println();
   }
 
   private static void actual(double[] s) {
     System.out.println("  Actual: " + Arrays.toString(s));
+  }
+
+  private static void expected(double[] s) {
+    System.out.println("Expected: " + Arrays.toString(s));
     System.out.println();
   }
 }
